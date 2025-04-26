768fc2d60684ba87eda0060be7229234af28c6dc
Yuri <142381932+Yurei04@users.noreply.github.com>
2025-04-26 17:26:24 +0800

    Deps(Animation, motion): adding deps. for UI

--- vox-aequalis/src/components/sections/homepage/home.jsx
+++ vox-aequalis/src/components/sections/homepage/home.jsx
@@ -5,6 +5,8 @@ import { SiteHeader } from "@/components/elements/site-header"
 import { Button } from "@/components/ui/button"
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 import Link from "next/link"
+import { TypeAnimation } from "react-type-animation"
+import { motion } from "motion/react"
 
 export default function Homepage() {
     return (
@@ -21,18 +23,9 @@ export default function Homepage() {
           <div className="flex flex-1 flex-col">
             <div className="@container/main flex flex-1 flex-col gap-2">
               <div className="flex flex-col items-center text-center justify-center gap-4 py-4 md:gap-6 md:py-6">
-                <div>
-                    <h1>Vox Aequalis</h1>
-                    <h4>Hello greetings there This is Vox Aequalis</h4>
-                </div>
-                <div className="flex flex-row items-center gap-3.5 ">
-                    <Button>
-                        <Link href={"#"}> Button 1 </Link>
-                    </Button>
-                    <Button>
-                        <Link href={"#"}> Button 2 </Link>
-                    </Button>
-                </div>
+                <motion.div>
+
+                </motion.div>
               </div>
             </div>
           </div>
