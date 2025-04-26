ca873e39b50a347b1bf218b263283e40ab840d7f
Yuri <142381932+Yurei04@users.noreply.github.com>
2025-04-26 17:16:39 +0800

    UI(dark mode): adding of dark mode feature

--- vox-aequalis/src/components/sections/homepage/home.jsx
+++ vox-aequalis/src/components/sections/homepage/home.jsx
@@ -2,7 +2,9 @@
 
 import AppSidebarMain from "@/components/elements/app-sidebar-main"
 import { SiteHeader } from "@/components/elements/site-header"
+import { Button } from "@/components/ui/button"
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
+import Link from "next/link"
 
 export default function Homepage() {
     return (
@@ -18,8 +20,19 @@ export default function Homepage() {
           <SiteHeader />
           <div className="flex flex-1 flex-col">
             <div className="@container/main flex flex-1 flex-col gap-2">
-              <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-
+              <div className="flex flex-col items-center text-center justify-center gap-4 py-4 md:gap-6 md:py-6">
+                <div>
+                    <h1>Vox Aequalis</h1>
+                    <h4>Hello greetings there This is Vox Aequalis</h4>
+                </div>
+                <div className="flex flex-row items-center gap-3.5 ">
+                    <Button>
+                        <Link href={"#"}> Button 1 </Link>
+                    </Button>
+                    <Button>
+                        <Link href={"#"}> Button 2 </Link>
+                    </Button>
+                </div>
               </div>
             </div>
           </div>
