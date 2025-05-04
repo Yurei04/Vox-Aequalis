a36eac001b591f4a015bfa438d59ee4a6ac18431
Yuri <142381932+Yurei04@users.noreply.github.com>
2025-05-04 21:46:55 +0800

    feat(chatbot): initialization chatbot

---
+++ vox-aequalis/src/components/sections/chat-bot/chat-bot.jsx
@@ -0,0 +1,17 @@
+"use client"
+
+export default function ChatBotAI () {
+    return (
+        <div className="flex flex-col w-full h-screen items-center">
+            <div>
+                <h1></h1>
+            </div>
+            <Card
+                className="w-full max"
+            
+            >
+
+            </Card>
+        </div>
+    )
+}
